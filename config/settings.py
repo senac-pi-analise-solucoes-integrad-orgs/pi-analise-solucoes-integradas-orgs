b4fcad27ad58e9464a62cd7642e8fcde4247665f
Gustavo Sousa <gustavo@gustavosousa.me>
2025-02-22 15:04:16 -0300

    Creating models on Core and setting email to unique on User model

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom User model
+
+AUTH_USER_MODEL = 'core.User'
