e982105397ee5e059f6f8a9a64703de446eaa8f1
luiz santos <153158308+luizfernandosant@users.noreply.github.com>
2025-02-26 18:34:21 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -9,3 +9,28 @@ class SpecialistViewSet(viewsets.ModelViewSet):
     serializer_class = SpecialistSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['user__name']
+
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        confirm_password = request.data.get('confirm_password')
+
+        if password != confirm_password:
+            return Response({"error": "As senhas não coincidem"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_user(username=username, email=email, password=password)
+            return Response({"message": "Usuário criado com sucesso"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": "Usuário já existe"}, status=status.HTTP_400_BAD_REQUEST)
