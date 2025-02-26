3782a421340c8489c901aea3bfeea209bb788892
luiz santos <153158308+luizfernandosant@users.noreply.github.com>
2025-02-26 18:35:19 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,15 @@ from core.views import SpecialistViewSet
 
 router = routers.DefaultRouter()
 router.register('specialists', SpecialistViewSet)
+from django.contrib import admin
+from django.urls import path
+from core.views import register_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+path('register/', register_user, name='register_user'),
     # path('api/api-token-auth/', rest_views.obtain_auth_token),
+
+
 ]
